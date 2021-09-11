d945a76d344c8307d8443c65dec263697c5c9082
sahin ogur <54636160+sahin88@users.noreply.github.com>
2021-09-11 23:55:45 +0200

    Delete kreatize_coding_challenge directory

--- kreatize_coding_challenge/src/actions/types.js
+++
@@ -1,4 +0,0 @@
-
-export const CREATE_POST = 'CREATE_POST';
-export const END_LOADING = 'END_LOADING';
-export const START_LOADING = "START_LOADING";
--- kreatize_coding_challenge/stlViwerApp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class StlviwerappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'stlViwerApp'
